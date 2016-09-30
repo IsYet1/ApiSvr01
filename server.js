2beeb586af8b29f52671ad6d58feff516d8de0ae
IsYet1 <don@isyet.com>
2016-09-30 11:02:57 -0500

    Add URL write.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var http = require('http');
 // Configure our HTTP Server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response){
     response.writeHead(200,{"Content-Type": "text/plain"});
+    response.write(request.url + "\n");
     response.end("Hello World Update 1. And update 2. And update 3\n");
 });
 
